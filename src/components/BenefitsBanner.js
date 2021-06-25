8661e23a7afd3e8faabf2288fbb032ed1eada9c8
Gabi <g.federico.rodriguez@gmail.com>
2021-06-25 13:57:16 -0300

    footer

--- src/components/BenefitsBanner.js
+++ src/components/BenefitsBanner.js
@@ -49,14 +49,14 @@ const BenefitsBanner = () => {
           viewBox="0 0 1440 400"
           preserveAspectRatio="none"
           xmlns="http://www.w3.org/2000/svg"
-          class="transition duration-300 ease-in-out delay-150"
+          className="transition duration-300 ease-in-out delay-150"
         >
           <path
             d="M 0,400 C 0,400 0,200 0,200 C 137.7333333333333,218.53333333333333 275.4666666666666,237.06666666666666 451,233 C 626.5333333333334,228.93333333333334 839.8666666666668,202.26666666666665 1011,193 C 1182.1333333333332,183.73333333333335 1311.0666666666666,191.86666666666667 1440,200 C 1440,200 1440,400 1440,400 Z"
             stroke="fill"
-            stroke-width="0"
+            strokeWidth="0"
             fill="#425389"
-            class="transition-all duration-300 ease-in-out delay-150"
+            className="transition-all duration-300 ease-in-out delay-150"
           ></path>
         </svg>
       </Decoration>
@@ -68,14 +68,14 @@ const BenefitsBanner = () => {
           viewBox="0 0 1440 400"
           preserveAspectRatio="none"
           xmlns="http://www.w3.org/2000/svg"
-          class="transition duration-300 ease-in-out delay-150"
+          className="transition duration-300 ease-in-out delay-150"
         >
           <path
             d="M 0,400 C 0,400 0,200 0,200 C 101.27272727272728,224.72727272727275 202.54545454545456,249.45454545454547 309,241 C 415.45454545454544,232.54545454545453 527.0909090909091,190.9090909090909 620,194 C 712.9090909090909,197.0909090909091 787.0909090909092,244.90909090909093 885,264 C 982.9090909090908,283.09090909090907 1104.5454545454545,273.4545454545455 1201,258 C 1297.4545454545455,242.54545454545453 1368.7272727272727,221.27272727272725 1440,200 C 1440,200 1440,400 1440,400 Z"
             stroke="none"
-            stroke-width="0"
+            strokeWidth="0"
             fill="#425389"
-            class="transition-all duration-300 ease-in-out delay-150"
+            className="transition-all duration-300 ease-in-out delay-150"
             transform="rotate(-180 720 200)"
           ></path>
         </svg>
@@ -189,7 +189,11 @@ const Decoration = styled.div`
   width: 100%;
   height: 101px;
 
-  @media screen and (max-width: 1343px) {
+  /* @media screen and (max-width: 1343px) {
+    top: -16%;
+  } */
+
+  @media screen and (max-width: 1360px) {
     top: -16%;
   }
 
@@ -205,21 +209,21 @@ const Decoration = styled.div`
 const DecorationTwo = styled.div`
   position: absolute;
   z-index: 0;
-  bottom: -24%;
+  bottom: -17%;
   right: 0;
   width: 100%;
-  height: 101px;
+  height: 70px;
 
-  @media screen and (max-width: 1343px) {
-    bottom: -16%;
+  @media screen and (max-width: 1360px) {
+    bottom: -11%;
   }
 
   @media screen and (max-width: 534px) {
-    bottom: -10%;
+    bottom: -7%;
   }
 
   @media screen and (max-width: 454px) {
-    bottom: -7%;
+    bottom: -6%;
   }
 `;
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,164 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <FooterWrapper>
-      <FooterContainer></FooterContainer>
+      <FooterContainer>
+        <GridContainer>
+          <ColumnContainer>
+            <Title>Institución</Title>
+            <Ul>
+              <Li>
+                <FooterLink to="/">Sobre nosotros</FooterLink>
+              </Li>
+              <Li>
+                <FooterLink to="/">Contacto</FooterLink>
+              </Li>
+              <Li>
+                <FooterLink to="/">Aviso legal y privacidad</FooterLink>
+              </Li>
+            </Ul>
+          </ColumnContainer>
+          <ColumnContainer>
+            <Title>Ofertas Top</Title>
+            <Ul>
+              <Li>
+                <FooterLink to="/">
+                  Tecnología, Sistemas y Telecomunicaciones
+                </FooterLink>
+              </Li>
+              <Li>
+                <FooterLink to="/">Salud, Medicina y Farmacia</FooterLink>
+              </Li>
+              <Li>
+                <FooterLink to="/">Diseño</FooterLink>
+              </Li>
+              <Li>
+                <FooterLink to="/">
+                  Administración, Contabilidad y Finanzas
+                </FooterLink>
+              </Li>
+              <Li>
+                <FooterLink to="/">Jóvenes Profesionales</FooterLink>
+              </Li>
+            </Ul>
+          </ColumnContainer>
+          <ColumnContainer>
+            <Title>Redes sociales</Title>
+            <Ul style={{ width: '140px' }}>
+              <Li>
+                <FooterLink to="/">
+                  <Linkedin /> LinkedIn
+                </FooterLink>
+              </Li>
+              <Li>
+                <FooterLink to="/">
+                  <Github /> Github
+                </FooterLink>
+              </Li>
+              <Li>
+                <FooterLink to="/">
+                  <Twitter /> Twitter
+                </FooterLink>
+              </Li>
+            </Ul>
+          </ColumnContainer>
+          <ColumnContainer>
+            <Title>Información relevante</Title>
+            <Ul>
+              <Li style={{ textAlign: 'justify' }}>
+                Este proyecto es un fake Job-bank con la finalidad de mostrar mi
+                trabajo. Favor de no ingresar información sensible. Disfrutalo.
+              </Li>
+            </Ul>
+          </ColumnContainer>
+        </GridContainer>
+      </FooterContainer>
     </FooterWrapper>
   );
 };
 
 const FooterWrapper = styled.div`
   width: 100%;
-  height: 400px;
-  background: #32b791;
+  background: #32b792;
 `;
 
 const FooterContainer = styled.div`
-  padding: 3rem calc((100vw - 1400px) / 2);
+  height: 100%;
+  padding: 9rem calc((100vw - 1400px) / 2) 3rem;
+`;
+
+const GridContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 0 2rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
+  grid-gap: 25px;
+`;
+
+const ColumnContainer = styled.div``;
+
+const Title = styled.h3`
+  font-family: 'Haid Vadodara', sans-serif;
+  font-weight: bold;
+  font-size: clamp(0.8rem, 2vw, 1.6rem);
+  color: #26335a;
+  margin-bottom: 1rem;
+`;
+
+const Ul = styled.ul``;
+
+const Li = styled.li`
+  font-family: 'Hind Vadodara', sans-serif;
+  font-weight: bolder;
+  color: #2a2b2c;
+  margin-bottom: 10px;
+`;
+
+const FooterLink = styled(Link)`
+  font-family: 'Hind Vadodara', sans-serif;
+  color: #2a2b2c;
+  display: flex;
+  align-items: center;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Linkedin = styled(FaLinkedin)`
+  color: #2a2b2c;
+  transition: 0.3s color ease-out;
+  font-size: 2rem;
+  margin-right: 1rem;
+  &:hover {
+    color: #505050;
+  }
+`;
+
+const Github = styled(FaGithub)`
+  color: #2a2b2c;
+  transition: 0.3s color ease-out;
+  margin-right: 1rem;
+  font-size: 2rem;
+  &:hover {
+    color: #505050;
+  }
+`;
+
+const Twitter = styled(FaTwitter)`
+  color: #2a2b2c;
+  transition: 0.3s color ease-out;
+  margin-right: 1rem;
+  font-size: 2rem;
+  &:hover {
+    color: #505050;
+  }
 `;
 
 export default Footer;
